649c0493761495d55e4f53fb433e51f86d29ad34
Camilo <User@192.168.80.85>
2025-01-14 21:23:40 -0500

    xD

---
+++ src/Modelo/Moto.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Moto {
+    private String serial;
+    private String modelo;
+    private int cilindraje;
+    private String tipoMoto;
+    private float precioUnitario;
+    private String color;
+
+    public Moto(String serial, String modelo, int cilindraje, String tipoMoto, float precioUnitario, String color) {
+        this.serial = serial;
+        this.modelo = modelo;
+        this.cilindraje = cilindraje;
+        this.tipoMoto = tipoMoto;
+        this.precioUnitario = precioUnitario;
+        this.color = color;
+    }
+    
+    
+}
