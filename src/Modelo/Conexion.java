a5bf97a9e66735ea5bc3d9e41de6153d57ddee8d
Camilo <User@192.168.80.85>
2025-01-15 19:06:02 -0500

    Dao

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -4,10 +4,29 @@
  */
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author User
  */
 public class Conexion {
-    
+    public Connection getConexion(){
+        Connection cnx = null;
+        String URL = "jdbc:mysql://localhost:3306/produccion";
+        String usuario = "root";
+        String contrasena = "12345678";  
+                
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cnx =  DriverManager.getConnection(URL,"root","");
+        }catch(SQLException|ClassNotFoundException ex){
+            System.err.println("Error al conectar la BD ->"+ex);
+            JOptionPane.showMessageDialog(null, "Error al conectar la BD!!!","conexion",1);
+        }   
+        return cnx;
+    }
 }
