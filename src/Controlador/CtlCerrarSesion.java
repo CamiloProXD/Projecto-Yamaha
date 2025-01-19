478b6f6d62468672b9971a9bf0e87a43e2e7635e
juanchispo <124466430+juanchispo@users.noreply.github.com>
2025-01-19 15:35:43 -0500

    Ctl Cerrar Sesion uwu

--- src/Controlador/CtlCerrarSesion.java
+++ src/Controlador/CtlCerrarSesion.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controlador;
 
-/**
- *
- * @author juanc
- */
-public class CtlCerrarSesion {
-    
+import Vista.CerrarSesion;
+import Vista.ViewAdministrador;
+import Vista.ViewPrincipal;
+import Vista.ViewVendedor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Modelo.Usuario;
+
+public class CtlCerrarSesion implements ActionListener {
+
+    private ViewPrincipal vp;
+    private ViewVendedor vuv;
+    private ViewAdministrador vpa;
+    private CerrarSesion cs;
+    private String Rol;
+
+    public CtlCerrarSesion(ViewPrincipal vp, ViewVendedor vuv, ViewAdministrador vpa, CerrarSesion cs, String Rol) {
+        this.cs = cs;
+        this.vpa = vpa;
+        this.vp = vp;
+        this.vuv = vuv;
+        this.cs.btncerrarSesion.addActionListener(this);
+        this.cs.btnSalir.addActionListener(this);
+        this.cs.btnMenuPrincipal.addActionListener(this);
+        this.Rol = Rol;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+
+        if (ev.getSource().equals(cs.btncerrarSesion)) {
+            vp.setVisible(true);
+            this.cs.dispose();
+        }
+        if (ev.getSource().equals(cs.btnSalir)) {
+            System.exit(0);
+        }
+        if (ev.getSource().equals(cs.btnMenuPrincipal)) {
+            if (Rol.equals("Admin")) {
+                this.vpa.setVisible(true);
+                this.cs.dispose();
+            }
+            if (Rol.equals("Empleado")) {
+                this.vuv.setVisible(true);
+                this.cs.dispose();              
+            }
+        }
+    }
 }
