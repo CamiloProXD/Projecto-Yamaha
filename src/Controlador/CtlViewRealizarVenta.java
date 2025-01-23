22ca89f1e62ebc4490a932d743f9176babb9c88e
LUIS A <LUIS A@DESKTOP-H25QQAM>
2025-01-22 21:04:47 -0500

    Catalogos

--- src/Controlador/CtlViewRealizarVenta.java
+++ src/Controlador/CtlViewRealizarVenta.java
@@ -190,44 +190,44 @@ public void generarFactura(int idVenta) {
         ps.setInt(1, idVenta);
         ResultSet rs = ps.executeQuery();
 
-        if (rs.isBeforeFirst()) {
-            String ruta = "D:\\universidad\\3er semestre\\proyecto Yamaha\\" + "Factura de la venta " + idVenta + ".pdf";
+        if (rs.next()) { 
+    String ruta = "D:\\universidad\\3er semestre\\proyecto Yamaha\\" + "Factura de la venta " + idVenta + ".pdf";
 
-            Document document = new Document();
-            PdfWriter.getInstance(document, new FileOutputStream(ruta));
-            document.open();
+    Document document = new Document();
+    PdfWriter.getInstance(document, new FileOutputStream(ruta));
+    document.open();
 
-            rs.next(); 
-            document.add(new Paragraph("Factura de Venta"));
-            document.add(new Paragraph("Número de Factura: " + rs.getInt("numero_factura")));
-            document.add(new Paragraph("Fecha: " + rs.getDate("fecha")));
-            document.add(new Paragraph("Cliente: " + rs.getString("nombres")));
-            document.add(new Paragraph("Cédula: " + rs.getString("persona_id")));
-            document.add(new Paragraph("Dirección: " + rs.getString("direccion")));
-            document.add(new Paragraph("Teléfono: " + rs.getString("numero_telefonico")));
-            document.add(new Paragraph("\n"));
+ 
+    document.add(new Paragraph("Factura de Venta"));
+    document.add(new Paragraph("Número de Factura: " + rs.getInt("numero_factura")));
+    document.add(new Paragraph("Fecha: " + rs.getDate("fecha")));
+    document.add(new Paragraph("Cliente: " + rs.getString("nombres")));
+    document.add(new Paragraph("Cédula: " + rs.getString("persona_id")));
+    document.add(new Paragraph("Dirección: " + rs.getString("direccion")));
+    document.add(new Paragraph("Teléfono: " + rs.getString("numero_telefonico")));
+    document.add(new Paragraph("\n"));
 
-            document.add(new Paragraph("Detalle de la Compra:"));
-            
-            rs.beforeFirst(); 
-            while (rs.next()) {
-                document.add(new Paragraph("Nombre de la moto: " + rs.getString("nombre")));
-                document.add(new Paragraph("Modelo: " + rs.getString("modelo")));
+    document.add(new Paragraph("Detalle de la Compra:"));
 
-                String precio = String.format("%.2f", rs.getDouble("precio_unitario"));
-                document.add(new Paragraph("Precio: $" + precio));
-                document.add(new Paragraph("\n"));
-            }
 
-            document.add(new Paragraph("¡Gracias por su compra!"));
-            document.close();
+    do {
+        document.add(new Paragraph("Nombre de la moto: " + rs.getString("nombre")));
+        document.add(new Paragraph("Modelo: " + rs.getString("modelo")));
 
-            Desktop.getDesktop().open(new File(ruta));
+        String precio = String.format("%.2f", rs.getDouble("precio_unitario"));
+        document.add(new Paragraph("Precio: $" + precio));
+        document.add(new Paragraph("\n"));
+    } while (rs.next());
 
-            JOptionPane.showMessageDialog(null, "Factura generada exitosamente", "Éxito", JOptionPane.INFORMATION_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(null, "No se encontraron datos para la factura " + idVenta, "Error", JOptionPane.ERROR_MESSAGE);
-        }
+    document.add(new Paragraph("¡Gracias por su compra!"));
+    document.close();
+
+    Desktop.getDesktop().open(new File(ruta));
+
+    JOptionPane.showMessageDialog(null, "Factura generada exitosamente", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+} else {
+    JOptionPane.showMessageDialog(null, "No se encontraron datos para la factura " + idVenta, "Error", JOptionPane.ERROR_MESSAGE);
+}
 
     } catch (Exception ex) {
         ex.printStackTrace();
